3751ecb695f7ff7186690b57369110d040f4a26b
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2021-10-31 14:28:44 +0000

    ci: add codecov

---
+++ .github/coverage/index.js
@@ -0,0 +1,7 @@
+var data = {files:[
+{"link":"run_coverage.af8280dcc6e14cb0/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"98.6","covered_lines":"72","uncovered_lines":"1","total_lines" : "73"},
+{"link":"run_coverage.8cacf39ad4c932ca/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
+], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineCov","covered":"97.1","covered_lines":"332","uncovered_lines":"10","total_lines" : "342"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "run_coverage", "date" : "2021-10-31 14:25:05", "instrumented" : 415, "covered" : 404,};
