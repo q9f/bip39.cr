7ea22a79ad06006faa87cdee83f0a44f262e073a
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-10-31 16:38:15 +0000

    docs: regenerate for release 0.1.0 (#6)
    * docs: regenerate for release 0.1.0
    
    * codecov: regenerate

--- .github/coverage/index.js
+++ .github/coverage/index.js
@@ -2,4 +2,4 @@ var data = {files:[
 {"link":"run_coverage.af8280dcc6e14cb0/index.html","title":"run_coverage","summary_name":"run_coverage","covered_class":"lineCov","covered":"98.6","covered_lines":"72","uncovered_lines":"1","total_lines" : "73"},
 ], merged_files:[]};
 var percent_low = 25;var percent_high = 75;
-var header = { "command" : "run_coverage", "date" : "2021-10-31 14:36:10", "instrumented" : 73, "covered" : 72,};
+var header = { "command" : "run_coverage", "date" : "2021-10-31 16:36:08", "instrumented" : 73, "covered" : 72,};
--- .github/coverage/run_coverage.af8280dcc6e14cb0/index.js
+++ .github/coverage/run_coverage.af8280dcc6e14cb0/index.js
@@ -3,5 +3,5 @@ var data = {files:[
 {"link":"bip39.cr.f1028fd6.html","title":"bip39.cr","summary_name":"[...]/bip39.cr/src/bip39.cr","covered_class":"lineCov","covered":"98.1","covered_lines":"53","uncovered_lines":"1","total_lines" : "54"},
 ]};
 var percent_low = 25;var percent_high = 75;
-var header = { "command" : "run_coverage", "date" : "2021-10-31 14:36:10", "instrumented" : 73, "covered" : 72,};
+var header = { "command" : "run_coverage", "date" : "2021-10-31 16:36:08", "instrumented" : 73, "covered" : 72,};
 var merged_data = [];
