beecb845f307f7c9ef3b947f8feb7e59af7f2522
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2021-10-31 15:53:03 +0000

    docs: add inline documenation

--- src/util.cr
+++ src/util.cr
@@ -12,43 +12,89 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-module Bip0039
-  class Util
-    # reads the bip-39 english wordlist from file
-    def self.bip0039_word_list : Array(String)
-      file_path = Path.new("dict", "english.txt")
-      list_file = File.read(file_path)
-      word_list = [] of String
-      list_file.each_line do |word|
-        word_list << word
-      end
-      return word_list
+# A collection of utilities for `Bip0039` mnemonic management, e.g.,
+# loading word lists from dictionaries, or n-bit padding operations.
+module Bip0039::Util
+  # A generic utility to load the standardized english language word list
+  # from file.
+  #
+  # ```
+  # Bip0039::Util.word_list
+  # # => ["abandon", "ability", "able", "about", "above", ...
+  # ```
+  def self.word_list : Array(String)
+    file_path = Path.new("dict", "english.txt")
+    list_file = File.read(file_path)
+    word_list = [] of String
+    list_file.each_line do |word|
+      word_list << word
     end
+    return word_list
+  end
 
-    # generates a padded binary string from hex with given size
-    def self.hex_to_padded_bin(hex : String, size : Int) : String
-      num = BigInt.new hex, 16
-      bin = num.to_s(2)
-      return bin.rjust size, '0'
-    end
+  # A generic utility to generate a padded binary string from a hex string
+  # with given size.
+  #
+  # Parameters:
+  # * `hex` (`String`): the hex string to be formatted as padded binary string.
+  # * `size` (`Int`): the size in bits for the resulting hex string, e.g., 128.
+  #
+  # ```
+  # Bip0039::Util.hex_to_padded_bin "69", 32
+  # # => "00000000000000000000000001101001"
+  # ```
+  def self.hex_to_padded_bin(hex : String, size : Int) : String
+    num = BigInt.new hex, 16
+    bin = num.to_s(2)
+    return bin.rjust size, '0'
+  end
 
-    # generates a padded binary string from a big number with given size
-    def self.num_to_padded_bin(num : BigInt | Int, size : Int) : String
-      bin = num.to_s(2)
-      return bin.rjust size, '0'
-    end
+  # A generic utility to generate a padded binary string from a big number
+  # with given size.
+  #
+  # Parameters:
+  # * `num` (`BigInt | Int`): the number to be formatted as padded binary string.
+  # * `size` (`Int`): the size in bits for the resulting hex string, e.g., 128.
+  #
+  # ```
+  # Bip0039::Util.num_to_padded_bin 137, 32
+  # # => "00000000000000000000000010001001"
+  # ```
+  def self.num_to_padded_bin(num : BigInt | Int, size : Int) : String
+    bin = num.to_s(2)
+    return bin.rjust size, '0'
+  end
 
-    # generates a padded hex string from a binary string with given size
-    def self.bin_to_padded_hex(bin : String, size : Int) : String
-      num = BigInt.new bin, 2
-      hex = num.to_s(16)
-      return hex.rjust (size / 4).to_i, '0'
-    end
+  # A generic utility to generate a padded hex string from a binary string
+  # with given size.
+  #
+  # Parameters:
+  # * `bin` (`String`): the binary string to be formatted as padded hex string.
+  # * `size` (`Int`): the size in bits for the resulting hex string, e.g., 128.
+  #
+  # ```
+  # Bip0039::Util.bin_to_padded_hex "1100101", 32
+  # # => "00000065"
+  # ```
+  def self.bin_to_padded_hex(bin : String, size : Int) : String
+    num = BigInt.new bin, 2
+    hex = num.to_s(16)
+    return hex.rjust (size / 4).to_i, '0'
+  end
 
-    # generates a padded hex string from a big number with given size
-    def self.num_to_padded_hex(num : BigInt | Int, size : Int) : String
-      hex = num.to_s(16)
-      return hex.rjust (size / 4).to_i, '0'
-    end
+  # A generic utility to generate a padded hex string from a big number
+  # with given size.
+  #
+  # Parameters:
+  # * `num` (`BigInt | Int`): the number to be formatted as padded hex string.
+  # * `size` (`Int`): the size in bits for the resulting hex string, e.g., 128.
+  #
+  # ```
+  # Bip0039::Util.num_to_padded_hex 137, 32
+  # # => "00000089"
+  # ```
+  def self.num_to_padded_hex(num : BigInt | Int, size : Int) : String
+    hex = num.to_s(16)
+    return hex.rjust (size / 4).to_i, '0'
   end
 end
