ab815f2976a22322def561b1f8e9b345ceb1461d
Afr Schoe <58883403+q9f@users.noreply.github.com>
2023-01-05 17:54:05 +0100

    rlp: happy new year

--- src/util.cr
+++ src/util.cr
@@ -1,4 +1,4 @@
-# Copyright 2021-2022 Afr Schoe
+# Copyright 2021-23 Afri Schoedon @q9f
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
